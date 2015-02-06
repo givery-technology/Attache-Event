4730fb35b0d80650abac95f3bb4c57db4679ebf5
maximum80 <dustbox.1218@gmail.com>
2015-02-06 16:38:39 +0900

    fixed navbar scroll

--- web/custom.js
+++ web/custom.js
@@ -1,38 +1,55 @@
+$(function(){
+    var box = $("#navbar");
+    var boxTop = box.offset().top;
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= boxTop){
+            box.addClass("fixed");
+            $("body").css("margin-top","80px");
+        }else{
+            box.removeClass("fixed");
+            $("body").css("margin-top","0px");
+        }
+    });
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top - 80;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
 
-
-
-// scrolling to page sections
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('#count1').animateNumber({
+      number: 48
+    }, 1000);
+    $('#count2').animateNumber({
+      number: 100
+    }, 1000);
+    $('#count3').animateNumber({
+      number: 30
+    }, 1000);
 });
 
-
-
-
-// tabs 
+// tabs
 $(document).ready(function() {
     /*jQuery('.tabs .tab-links a').on('click', function(e)  {*/
-	
+
 	$('.tab-links a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
- 
+
         // Show/Hide Tabs
         $('.tabs ' + currentAttrValue).show().siblings().hide();
- 
+
         // Change/remove current tab to active
         $(this).parent('li').addClass('active').siblings().removeClass('active');
- 
+
         e.preventDefault();
     });
 });
+
