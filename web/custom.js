396af7be98318f1f03645852cc0263cb0cef11de
shunjikonishi <shun.konishi@nifty.com>
2015-02-06 11:36:36 +0900

    Initail commit

---
+++ web/custom.js
@@ -0,0 +1,38 @@
+
+
+
+// scrolling to page sections
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+// tabs 
+$(document).ready(function() {
+    /*jQuery('.tabs .tab-links a').on('click', function(e)  {*/
+	
+	$('.tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        $('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+});
